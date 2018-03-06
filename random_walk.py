0beb14864adf94bb907edd9c4f75f523452e1696
east4ming <cuikaidong@foxmail.com>
2018-03-06 10:40:59 +0800

    20180305: 散点图细节调整; 随机漫步方法重构

--- random_walk.py
+++ random_walk.py
@@ -1,5 +1,6 @@
 from random import choice
 
+
 class RandomWalk():
     """一个生成随机漫步数据的类"""
 
@@ -11,27 +12,24 @@ class RandomWalk():
         self.x_values = [0]
         self.y_values = [0]
 
+    def get_step(self):
+        # 决定前进方向以及演这个方向前进的距离
+        direction = choice([-1, 1])
+        distance = choice(list(range(9)))
+        step = direction * distance
+        return step
+
     def fill_walk(self):
         """计算随机漫步包含的所有点"""
-
         # 不断漫步, 知道列表达到指定的长度
         while len(self.x_values) < self.num_points:
-            # 决定前进方向以及演这个方向前进的距离
-            x_direction = choice([-1, 1])
-            x_distance = choice([0, 1, 2, 3, 4])
-            x_step = x_direction * x_distance
-
-            y_direction = choice([-1, 1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
-
+            x_step = self.get_step()
+            y_step = self.get_step()
             # 拒绝原地踏步
             if x_step == 0 and y_step == 0:
                 continue
-
             # 计算下一个点的x和y值
             next_x = self.x_values[-1] + x_step
             next_y = self.y_values[-1] + y_step
-
             self.x_values.append(next_x)
             self.y_values.append(next_y)
--- rw_visual.py
+++ rw_visual.py
@@ -6,7 +6,22 @@ from random_walk import RandomWalk
 while 1:
     rw = RandomWalk()
     rw.fill_walk()
-    plt.scatter(rw.x_values, rw.y_values, s=15)
+
+    # 设置绘图窗口的尺寸
+    plt.figure(figsize=(10, 6))
+
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, s=1, c=point_numbers,
+                cmap=plt.cm.Blues)
+    # plt.plot(rw.x_values, rw.y_values, linewidth=1)
+
+    # 突出起点和终点
+    plt.scatter(0, 0, c='green', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', s=100)
+
+    # 隐藏坐标轴
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
